be54044d27446260ff21796d768ee69f0b62fc51
AhmedAdelSalama <48366725+AhmedAdelSalama@users.noreply.github.com>
2021-05-02 15:07:31 +0200

    Update RegularDefinition.h

--- def/RegularDefinition.h
+++ def/RegularDefinition.h
@@ -1,9 +1,52 @@
-#include "./def/Imports.h"
+#ifndef _RegularDefinition_
+#define _RegularDefinition_
+
+#include "Imports.h"
 
 class RegularDefinition
 {
 
     public:
-
+        void set_value(string val)
+        {
+            value = val;
+        }
+        void append_to_redef_value(string str)
+        {
+            if (is_value_empty())
+                value = str;
+            else
+                value = value + "|" + str;
+        }
+        bool is_value_empty()
+        {
+            return value == "";
+        }
+        string get_value()
+        {
+            return value;
+        }
+        void set_type(string ty)
+        {
+            type = ty;
+        }
+        string get_type()
+        {
+            return type;
+        }
+        void add_to_all_possible_chars(vector<char> elements)
+        {
+            for (int i = 0; i < elements.size(); i++) {
+                all_possible_chars.push_back(elements[i]);
+            }
+        }
+        vector<char> get_to_all_possible_chars()
+        {
+            return all_possible_chars;
+        }
     private:
+        string value;
+        string type;
+        vector<char> all_possible_chars;
 };
+#endif
