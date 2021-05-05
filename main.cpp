5b69d2c6328ade96e1697fa4cd57603263a6835e
unknown <mahmoud.manfy159@gmail.com>
2021-05-05 16:41:45 +0200

    add main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::string input = "a|b&c";
+    std::string input = "0&1|0&0";
     NFA_Generator* nfaGenerator = new NFA_Generator;
     Graph* graph = nfaGenerator->to_NFA(input);
     graph->print();
